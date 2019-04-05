0075ff4bfc977b7bf72782227877b9e558bbedbe
Flubz <FarjadM96@gmail.com>
2019-04-05 13:58:51 +0100

    ChunkGenerationAdded
    Chunk generation added.
    Removed Proc gen meshes.

---
+++ Source/FMAGI/Private/MainPlayerController.cpp
@@ -0,0 +1,58 @@
+#include "MainPlayerController.h"
+#include "Runtime/Engine/Classes/Kismet/KismetMathLibrary.h"
+
+void AMainPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	_chunkSize = _chunkLineElement * _voxelSize;
+	_chunkSizeHalf = _chunkSize / 2;
+}
+
+void AMainPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UpdatePosition();
+	GenerateChunk();
+}
+
+bool AMainPlayerController::UpdatePosition()
+{
+	FVector pos = GetPawn()->GetActorLocation() * FVector(1, 1, 0);
+	_characterPosition = pos;
+
+	FVector2D flooredPos = FVector2D(UKismetMathLibrary::FFloor(pos.X / (float) _chunkSizeHalf), UKismetMathLibrary::FFloor(pos.Y / (float) _chunkSizeHalf));
+
+	if (flooredPos.X != _chunkX || flooredPos.Y != _chunkY)
+	{
+		_chunkX = flooredPos.X;
+		_chunkY = flooredPos.Y;
+		return true;
+	}
+
+	return false;
+}
+
+void AMainPlayerController::GenerateChunk()
+{
+	FActorSpawnParameters spawnParams = FActorSpawnParameters();
+	spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	FRotator rot = FRotator(0, 0, 0);
+	FVector spawnPos = FVector(0, 0, 0);
+	FVector2D chunkPos = FVector2D();
+
+	for (int x = -_renderRange; x <= _renderRange; x++)
+	{
+		for (int y = -_renderRange; y <= _renderRange; y++)
+		{
+			chunkPos.X = _chunkX + x;
+			chunkPos.Y = _chunkY + y;
+			if (!_chunksCords.Contains(chunkPos))
+			{
+				_chunksCords.Add(chunkPos);
+				spawnPos = FVector(chunkPos.X * _chunkSize, chunkPos.Y  * _chunkSize, 0);
+				AActor* chunk = GetWorld()->SpawnActor<AActor>(_chunk, spawnPos, rot, spawnParams);
+				_chunks.Add(chunk);
+			}
+		}
+	}
+}
---
+++ Source/FMAGI/Public/MainPlayerController.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+UCLASS()
+class FMAGI_API AMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(Category = Settings, EditAnywhere, BlueprintReadWrite) int32 _renderRange;
+	UPROPERTY(Category = Settings, EditAnywhere, BlueprintReadWrite) int32 _chunkLineElement;
+	UPROPERTY(Category = Settings, EditAnywhere, BlueprintReadWrite) int32 _voxelSize;
+	// Is ChunkLineElement * VoxelSize
+	UPROPERTY(Category = Settings, VisibleAnywhere, BlueprintReadOnly) int32 _chunkSize;
+	UPROPERTY(Category = Settings, VisibleAnywhere, BlueprintReadOnly) int32 _chunkSizeHalf;
+	UPROPERTY(Category = Settings, EditAnywhere, BlueprintReadWrite) int32 _chunkX;
+	UPROPERTY(Category = Settings, EditAnywhere, BlueprintReadWrite) int32 _chunkY;
+	UPROPERTY(Category = Settings, EditAnywhere, BlueprintReadWrite) FVector _characterPosition;
+	UPROPERTY(Category = Settings, EditAnywhere, BlueprintReadWrite) TSubclassOf<class AActor> _chunk;
+	TArray<AActor*> _chunks;
+	TArray<FVector2D> _chunksCords;
+
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	bool UpdatePosition();
+	void GenerateChunk();
+
+};
